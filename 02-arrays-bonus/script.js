363d4296c611d3ff5408fd0d018f031338eb7ae5
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-10-30 14:20:33 +0100

    feat: bonus exercise completed

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,19 +1,35 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+for (let i = 0; i < teachers.length; i++) {
+  console.log(teachers[i]);
+}
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
-
+const longNames = teachers.filter(checkCharacters);
+function checkCharacters(varIngresso) {
+  return varIngresso.length >= 5;
+}
+for (let i = 0; i < longNames.length; i++) {
+  console.log(`${longNames[i]} contiene 5 o più caratteri.`);
+}
 // 3. Rimuovi 'Ed' dall'array teachers
+
+// Ciclo l'array per controllare gli elementi prima di rimouverne uno
+for (let i = 0; i < teachers.length; i++) {
+  console.log(`PRIMA => All'indice: ${i} c'è ${teachers[i]}`);
+}
+
+let indexElemento = teachers.indexOf("Ed");
+
+// Ciclo l'array per controllare gli elementi dopo aver rimosso 'Ed'
+teachers.splice(indexElemento, 1);
+for (let i = 0; i < teachers.length; i++) {
+  console.log(`DOPO => All'indice: ${i} c'è ${teachers[i]}`);
+}
+
+const indexEd = teachers.includes('Ed');
+console.log("Is Ed in the array ? " + indexEd);
